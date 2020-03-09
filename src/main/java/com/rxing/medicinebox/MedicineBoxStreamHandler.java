fe59a2cff1ad00a88298663df71f1518434e5cc0
anniepineda <anniepineda@outlook.com>
2020-03-09 16:35:33 -0700

    list all commands and welcome message

--- src/main/java/com/rxing/medicinebox/MedicineBoxStreamHandler.java
+++ src/main/java/com/rxing/medicinebox/MedicineBoxStreamHandler.java
@@ -13,7 +13,8 @@ public class MedicineBoxStreamHandler extends SkillStreamHandler {
                         new LaunchRequestHandler(),
                         new AddMedicationIntentHandler(),
                         new GetSingleMedicationIntentHandler(),
-                        new GetAllMedicationsIntentHandler())
+                        new GetAllMedicationsIntentHandler(),
+                        new ListAllCommandsIntentHandler())
                 .withSkillId("amzn1.ask.skill.9f64aeb9-c2dd-4989-8f9c-06aae7f38282")
                 .withTableName("MedicineBox")
                 .withAutoCreateTable(true)
