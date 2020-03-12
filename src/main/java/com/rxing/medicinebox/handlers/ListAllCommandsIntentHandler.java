3c6bad27a7923c305a4df75e7b58594c3ed35fbd
Phong <phongdoan.dev@gmail.com>
2020-03-12 16:17:37 -0700

    corrected some dialog

--- src/main/java/com/rxing/medicinebox/handlers/ListAllCommandsIntentHandler.java
+++ src/main/java/com/rxing/medicinebox/handlers/ListAllCommandsIntentHandler.java
@@ -20,11 +20,11 @@ public class ListAllCommandsIntentHandler implements RequestHandler {
     @Override
     public Optional<Response> handle(HandlerInput handlerInput) {
         StringBuilder speechText = new StringBuilder();
-        speechText.append("Welcome to your virtual medicine box.  To use this prescription calling service, here's a list of available commands you can say.");
-        speechText.append("If you want to add a medication to your box, say add a medication.");
-        speechText.append("If you want to get details about a single medication, say get one med.");
-        speechText.append("If you want to get a list of all your medications in your box, say list every medications.");
-        speechText.append("If you want to create a reminder for your medication, say add a reminder.");
+        speechText.append("Welcome to your virtual medicine box,  To use this prescription calling service, here's a list of available commands you can say");
+        speechText.append("If you want to add a medication to your box, say add a medication");
+        speechText.append("If you want to get details about a single medication, say get one med");
+        speechText.append("If you want to get a list of all your medications in your box, say list every medications");
+        speechText.append("If you want to create a reminder for your medication, say add a reminder");
         return handlerInput.getResponseBuilder()
                 .withSpeech(speechText.toString())
                 .withShouldEndSession(false)
